ba882b85e3e19c2f9fe8bc6b854d867bdad7f378
walacefex <walacefex01@gmail.com>
2023-03-30 15:32:09 -0300

    adding parameters component heading

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -15,4 +15,12 @@ export const CustomTag: StoryObj<HeadingProps> = {
     children: 'H1 Heading',
     as: 'h1',
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Por padrão o heading sempre será um  `h2`, mas podemos alterar isso com a propriedade `as`',
+      },
+    },
+  },
 }
